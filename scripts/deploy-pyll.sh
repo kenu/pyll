4e266f8093178ef2b4aaca4ce03f4f01d063aea4
kenu <kenu@okjsp.pe.kr>
2026-01-31 11:16:23 +0900

    deploy

--- scripts/deploy-pyll.sh
+++ scripts/deploy-pyll.sh
@@ -3,3 +3,4 @@ cd ~/git/pyll
 git pull origin main
 docker compose down
 docker compose up --build -d
+curl -X POST -H 'Content-type: application/json' --data '{"content":"⏱️ Deploy Job Finished!\nhttps://justdev.net/"}' $DISCORD_WEBHOOK
